ba01dbd4e1c00c89a65d337fbb3b31cd0a8993ae
PR0TT1 <fabriprotti97@gmail.com>
2025-10-21 07:29:39 -0600

    Creación de proyecto

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require_once "./vistas/vista_superior.php"; ?>
+
+vista principal
+
+
+<?php require_once "./vistas/vista_inferior.php"; ?>
---
+++ vistas/vista_superior.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<meta charset="utf-8">
+<meta name="Tercera 1 proyecto" content="Proyecto de la tercera 1">
+<title>Servicios Profesionales</title>
+<link rel="stylesheet" href="./css/main.css">
+
+
+
+</head>
+
+
